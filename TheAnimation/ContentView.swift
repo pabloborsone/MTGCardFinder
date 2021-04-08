11f923cc90b726fd1d984916995e928fc6d90e66
Pablo Borsone <pablo.borsone.ext@panonline.com.br>
2021-04-08 11:10:38 -0300

    Initial Commit

---
+++ TheAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TheAnimation
+//
+//  Created by Pablo Borsone on 08/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
